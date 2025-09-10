5d17d85668b760b5c67a7402551fad89e127f8a0
Ondřej Berčík <ondrej.bercik@ice.cz>
2025-09-10 14:13:58 +0200

    Feature: add section devider

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,9 @@ import PriceListSection from "./price-list-section";
 
 export default function Home() {
   return <div>
+    <div className="w-full h-0.5 py-8 bg-bg-color">
+      <div className="border-b-2 border-text"></div>
+    </div>
     <PriceListSection />
     <GallerySection />
     <ContactSection />
