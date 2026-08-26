b27d6bf47d190f83a9ef51a19ef3cb101601c257
Ondřej Berčík <ondrej.bercik@gmail.com>
2026-08-26 17:49:48 +0200

    update: new price list

--- app/price-list-section.tsx
+++ app/price-list-section.tsx
@@ -1,34 +1,40 @@
-import SectionTitle from '@/components/section-title'
-import path from 'path'
+import SectionTitle from '@/components/section-title';
+import path from 'path';
 import fs from 'fs';
 import { parse } from 'csv-parse/sync';
 import PriceList from '@/components/price-list/price-list';
 import { HEADER_MAP, Wood } from '@/utils/price-type';
 
 const PriceListSection = () => {
-
-  const filePath = path.join(process.cwd(), 'public', 'data', 'Cenik.csv') 
+  const filePath = path.join(process.cwd(), 'public', 'data', 'Cenik.csv');
   const fileContent = fs.readFileSync(filePath, 'utf8');
   const data: Wood[] = parse(fileContent, {
     delimiter: ';',
-    columns: (headers) => 
-      headers.map(h => HEADER_MAP[h] ?? h),
+    columns: (headers) => headers.map((h) => HEADER_MAP[h] ?? h),
     skip_empty_lines: true,
-    trim: true
-  })
+    trim: true,
+  });
 
   return (
-    <section className='w-full bg-bg-color py-8 p-4 md:p-8' id='section--price-list'>
+    <section
+      className='w-full bg-bg-color py-8 p-4 md:p-8'
+      id='section--price-list'
+    >
       <div className='w-full mx-auto max-w-6xl flex flex-col gap-8'>
         <div>
           <SectionTitle title='Ceník' />
-          <h3 className='text-primary font-bold text-2xl'>Suchého palivového dříví</h3>
-          <p>Naše dřevo je v suchém stavu k okamžitému použití</p>
+          <h3 className='text-primary font-bold text-2xl'>
+            Suchého palivového dříví
+          </h3>
+          <p>
+            Naše dřevo je v suchém stavu k okamžitému použití. (Do vyprodání
+            skladových zásob.)
+          </p>
         </div>
         <PriceList data={data} />
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default PriceListSection
+export default PriceListSection;
